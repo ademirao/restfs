c8abaf130291db667b4e429939f910b824b9868a
Ademir Oliveira <ademirao@gmail.com>
2021-12-17 14:16:18 -0300

    Remover test diretorio

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ademirao/workspace/restfs
-# Build directory: /home/ademirao/workspace/restfs/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/ademirao/workspace/restfs/test/path_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
