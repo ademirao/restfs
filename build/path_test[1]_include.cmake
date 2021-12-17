4522eec7339453e6cdaebe3d00fc38dfec3a706f
Ademir Oliveira <ademirao@gmail.com>
2021-12-17 14:15:17 -0300

    Remover build diretorio

--- build/path_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/ademirao/workspace/restfs/build/path_test[1]_tests.cmake")
-  include("/home/ademirao/workspace/restfs/build/path_test[1]_tests.cmake")
-else()
-  add_test(path_test_NOT_BUILT path_test_NOT_BUILT)
-endif()
